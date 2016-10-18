22334769951567c7a5ad53b989669105e7ad7f4f
nsmeds <niilosmeds@gmail.com>
2016-10-18 15:06:01 -0700

    Used node-emoji instead of continuing to spend valuable hours trying to make asciify testable. Added dependencies and scripts in package.json

--- test/greet.test.js
+++ test/greet.test.js
@@ -1,26 +1,16 @@
 var greet = require('../lib/greet');
 var assert = require('assert');
-var asciify = require('asciify');
+var emoji = require('node-emoji').emoji;
 
 describe('greeter', function() {
-  var asciified = {};
 
   it('greet with name', function() {
     var greeting = greet('test');
-    assert.equal(greeting, 'hello test, here is your name, red and asciified!');
+    assert.equal(greeting, 'hello test ' + emoji.v);
   });
 
   it('greet if no name', function() {
     var greeting = greet();
-    assert.equal(greeting, 'hello my new friend, here is your name, red and asciified!');
-  });
-
-  it('greet in ascii', function() {
-    var greeting = greet('asciiTest');
-    asciify('asciiTest',{color:'red'},function(err,res){
-      console.log(res);
-      asciified.ascii = res;
-    });
-    assert.equal(greeting, asciified.ascii);
+    assert.equal(greeting, 'hello my new friend ' + emoji.v);
   });
 });
